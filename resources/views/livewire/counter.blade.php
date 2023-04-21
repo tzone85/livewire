ee5862aeed586bbe49ac767a7129a20443ba8879
Thando Mini <tzone85@gmail.com>
2023-04-21 23:11:31 +0200

    added tailwind css with some configs, then  added styling to my component

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,6 +1,6 @@
-<div>
+<div class="p-16 flex justify-center gap-6 items-center">
     {{-- The best athlete wants his opponent at his best. --}}
-    <button wire:click="increment">+</button>
+    <button wire:click="increment" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">+</button>
     <span>{{ $count }}</span>
-    <button wire:click="decrement">-</button>
+    <button wire:click="decrement" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">-</button>
 </div>

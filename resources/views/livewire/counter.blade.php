b579afc8c151ad0137716fa67e2ffee95106a5c1
Thando Mini <tzone85@gmail.com>
2023-04-21 22:42:55 +0200

    created a livewire counter component

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div>
+    {{-- The best athlete wants his opponent at his best. --}}
+    <button wire:click="increment">+</button>
+    <span>{{ $count }}</span>
+    <button wire:click="decrement">-</button>
+</div>

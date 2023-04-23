d592b56d3c8879ee3d76a6df3718d1a648e8c738
Thando Mini <tzone85@gmail.com>
2023-04-23 15:13:12 +0200

    created a new component, viz, cascadig dropdown. Then two models with factories for continents and countries

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Livewire\Calculator;
+use App\Http\Livewire\CascadingDropdown;
 use App\Http\Livewire\TodoList;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,5 @@ Route::get('/counter', function () {
 })->name('counter');
 
 Route::get('/calculator', Calculator::class)->name('calculator');
-Route::get('todo-list', TodoList::class)->name('todo-list');
+Route::get('/todo-list', TodoList::class)->name('todo-list');
+Route::get('/cascading-dropdown', CascadingDropdown::class)->name('cascading-dropdown');
